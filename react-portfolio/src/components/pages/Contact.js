f5b3ac9d54299e0467818348798d7aa3c8a55b00
saysa <saysaygood83@gmail.com>
2023-10-24 16:02:22 +0100

    Rendered Home and Contact page

---
+++ react-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+      <div className="text-bg-info p3 text-center mb-3 font-monospace">
+             <h1>Contact Me</h1>
+         </div>
+    <div className="text-info p3 text-center mb-3 font-monospace">
+             <h2>Saynab Good</h2>
+        </div>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/8740/8740316.png" class="rounded mx-auto d-block" width="20%" alt="image of a cartoon cat"></img>
+      </div>
+      <div className="text-info p3 text-center mb-3 font-monospace">
+        <p>
+        Here are ways you can contact me! 
+        </p>
+      </div>
+      <div>
+      <a class="btn btn-info btn-lg d-grid gap-2 col-6 mx-auto m-3 text-white" href="mailto:saysaygood83@gmail.com.com" role="button">Gmail</a>
+      </div>
+      <div>
+      <a class="btn btn-info btn-lg d-grid gap-2 col-6 mx-auto m-3 text-white" href="https://github.com/saycatnab" role="button">Github</a>
+      </div>
+      <div>
+      <a class="btn btn-info btn-lg d-grid gap-2 col-6 mx-auto m-3 text-white" href="https://www.linkedin.com/in/saynab-good-49432b269/" role="button">LinkedIn</a>
+      </div>
+    </div>
+    )
+    
+}
+//   <div>
+//     
+//     <div className="text-info p3 text-center mb-3 font-monospace">
+//         <h2>Saynab Good</h2>
+//     </div>
+//     <div>
+//     <p>
+
+//     </p>
+//     </div>
+//   </div>
+
+
+export default Contact;
---
+++ react-portfolio/src/components/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="text-bg-info p3 text-center mb-3 font-monospace">
+        <h1>Home Page</h1>
+      </div>
+      <div className="text-info p3 text-center mb-3 font-monospace">
+             <h2>Saynab Good</h2>
+        </div>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/8740/8740316.png" class="rounded mx-auto d-block" width="20%" alt="image of a cartoon cat"></img>
+      </div>
+      <div className="text-info p3 text-center mb-3 font-monospace">
+        <p>
+        Hello! Welcome to my portfolio site, please look around to see my Projects and Contact info! 
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
